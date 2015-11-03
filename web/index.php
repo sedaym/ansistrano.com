7d9034e6d27a2fb5fff4b894182c270c6bb4bcde
Carlos Buenosvinos <carlos.buenosvinos@gmail.com>
2015-11-03 10:57:38 +0100

    Fix home loading

--- web/index.php
+++ web/index.php
@@ -1,6 +1,8 @@
 <?php
 
 use Ansistrano\DeploymentsCounter;
+use Ansistrano\RedisStatsRepository;
+use Predis\Client;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 require_once __DIR__.'/../vendor/autoload.php';
@@ -16,17 +18,20 @@ $app->register(new Silex\Provider\TwigServiceProvider(), array(
 $app['now'] = new \DateTimeImmutable('now', new \DateTimeZone('UTC'));
 
 $app['redis'] = $app->share(function() {
-    return new \Predis\Client();
+    return new Client();
 });
 
 $app['stats_repository'] = $app->share(function($app) {
-    return new \Ansistrano\RedisStatsRepository($app['redis']);
+    return new RedisStatsRepository($app['redis']);
+});
+
+$app['deployments_counter'] = $app->share(function($app) {
+    return new DeploymentsCounter($app['stats_repository']);
 });
 
 $app->post('/deploy', function() use($app) {
     try {
-        $dc = new DeploymentsCounter($app['stats_repository']);
-        $dc->addDeployment($app['now']);
+        $app['deployments_counter']->addDeployment($app['now']);
     } catch(\Exception $e) {
 
         $res = ['error' => true];
@@ -41,7 +46,7 @@ $app->post('/deploy', function() use($app) {
 });
 
 $app->get('/', function() use($app) {
-    return $app['twig']->render('home.twig', (new DeploymentsCounter($app['redis']))->statsFor($app['now']));
+    return $app['twig']->render('home.twig', $app['deployments_counter']->statsFor($app['now']));
 });
 
 $app->run();
